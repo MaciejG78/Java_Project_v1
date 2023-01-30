f84d467e8ffcec6e344c069406c2a9c88766326b
MaciejG78 <Maciej_G@poczta.fm>
2023-01-30 20:35:37 +0100

    AppJoin

--- src/KodillaLabs/AppPositionChange/AppPositionChange.java
+++ src/KodillaLabs/AppPositionChange/AppPositionChange.java
@@ -24,20 +24,20 @@ class Numbers {
 
     public void changePositionWithTmp() {
         System.out.println("Sposób 1 z dodatkową zmienną:");
-        System.out.println("Before chaning position: a = " + this.a + ", b = " + this.b);
+        System.out.println("Before changing position: a = " + this.a + ", b = " + this.b);
         int tmp = this.a;
         this.a = this.b;
         this.b = tmp;
-        System.out.println("After chaning position: a = " + this.a + ", b = " + this.b);
+        System.out.println("After changing position: a = " + this.a + ", b = " + this.b);
     }
 
     public void changePositionWithoutTmp() {
         System.out.println("Sposób 2 bez dodatkowej zmiennej:");
-        System.out.println("Before chaning position: a = " + this.a + ", b = " + this.b);
+        System.out.println("Before changing position: a = " + this.a + ", b = " + this.b);
         this.a += this.b;  // wynik (A=a) => (A) a+b
         this.b = this.a - this.b; // wynik (B=b) => (a+b)-b = a
         this.a -= this.b; // wynik (A) => (a+b)-a = b
-        System.out.println("After chaning position: a = " + this.a + ", b = " + this.b);
+        System.out.println("After changing position: a = " + this.a + ", b = " + this.b);
     }
 }
 
---
+++ src/KodillaLabs/ComparingDouble/ComparingDouble.java
@@ -0,0 +1,15 @@
+package KodillaLabs.ComparingDouble;
+
+public class ComparingDouble {
+
+    public static void main(String[] args) {
+        double a = 41.7785432;
+        double b = 41.67864322;
+
+        System.out.println("Compare to 3 decimal places: " + a + " = " + b + " => " + compare(a , b));
+    }
+
+    public static boolean compare(double a, double b) {
+        return (Math.floor((a*1000)/1) == Math.floor((b*1000)/1));
+    }
+}
